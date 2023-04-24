8156bd6d4bed22e09a0c816e81df1c798ecacfc1
GOOSxE <der.goose.4308@gmail.com>
2023-04-24 18:22:40 +0200

    worked on js-hw-07 ex-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListUrl = document.querySelector(".gallery");
+// *
+
+// ? // Створюю масив з li>a>img> методом map та додаю розмітку в html
+
+const newGalleryItems = galleryItems
+  .map(
+    (image) =>
+      `<li class=gallery__item><a class=gallery__link href=${image.original}><img class=gallery__image src=${image.preview} data-source=${image.original} alt='${image.description}'></img></a></li>`
+  )
+  .join("");
+
+galleryListUrl.insertAdjacentHTML("afterbegin", newGalleryItems);
+
+// ? // Створюю модалку за допомогою SimpleLightBox
+
+new SimpleLightbox(".gallery a", {
+  overlayOpacity: 0.6,
+  captionsData: "alt",
+  captionDelay: 250,
+});
+// *
